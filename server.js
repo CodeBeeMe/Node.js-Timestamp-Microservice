f585e76c81468086ab155efd90a4def1afcccc29
Cătălin Anghel-Ursu <cat.ang.dev@gmail.com>
2019-01-17 14:27:03 +0200

    Update server.js
    Accessing the date parameter from the route /api/timestamp/:date? correctly via req.params.date and not the date query string using req.query.date as I wrongly used before. I was confused about how to access a named route parameter, in this case the "date" parameter. The confusion stemmed from the date_string description in the user stories section.
    I've cleaned up the code a bit and added a few comments for future reference and for a clearer picture.
    The project is now complete, fully functional and ready to be submitted to FCC for review.

--- server.js
+++ server.js
@@ -34,23 +34,31 @@ var listener = app.listen(process.env.PORT, function () {
 //=============== My code below ===================
 
 app.get('/api/timestamp/:date?', (req, res) => {
-
-  let dateQuery = req.query.date;
   
-  if(dateQuery === undefined) {
-    req.utcTime = new Date().toUTCString();
-    req.unixTime = new Date().getTime();
-  } else {
-    if(!isNaN(dateQuery)) {
-      req.unixTime = Number(dateQuery);
-      req.utcTime = new Date(Number(dateQuery)).toUTCString();
-    } else {
-      req.unixTime = Date.parse(dateQuery);
-      req.utcTime = new Date(dateQuery).toUTCString();
-    }
+  console.log(req.params.date);
+      
+  let dateString = req.params.date; // accessing the date parameter
+  
+  function currentDate() { //if dateQuery variable is empty or undefined return the current date
+    req.millisecs = new Date().getTime();
+    req.fullDate = new Date().toUTCString();
   }
   
-  let timeStamp = { unix: req.unixTime, utc: req.utcTime };  
-  res.json(timeStamp);  
+  function unixToUtc() { //in case it's a unix string, convert the string into a number using Number() or parseInt()
+    req.millisecs = Number(dateString);
+    req.fullDate = new Date(Number(dateString)).toUTCString();
+  }
+  
+  function utcToUnix() { //in case it's not a unix string, convert the string representation of a date to the number of milliseconds since January 1, 1970, 00:00:00 UTC
+    req.millisecs = Date.parse(dateString); 
+    req.fullDate = new Date(dateString).toUTCString();
+  }
+  
+  //using the above corresponding functions, applying the correct date conversion based on the nature of dateString variable
+  dateString === undefined ? currentDate() : (!isNaN(dateString) ? unixToUtc() : utcToUnix()); 
+  
+  res.json({ unix: req.millisecs, utc: req.fullDate }); // the timestamp object
+  
+  //console.log(isNaN(dateString));
+  
 });
-
